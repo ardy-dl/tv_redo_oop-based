f1dd3071ccae6ecffc5b181f621bdd892cf97d7e
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-20 13:11:47 +0800

    Main File Pseudocode

---
+++ tv_driver.py
@@ -0,0 +1,8 @@
+# create separate file for UI
+# create separate file for TV functions
+
+# create pseudocode for ui file
+# create pseudocode for Tv functions
+# create objects
+# call the methods using the objects
+# create UI designs
